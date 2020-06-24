4863565d792eeab1d31dbe1bb2e228799c6c98a6
yueyinqiu <lujiaqi2003@126.com>
2020-06-24 17:28:34 +0800

    优化entities

--- NWorldPermissions/src/top/nololiyt/worldpermissions/entities/DotDividedStringBuilder.kt
+++ NWorldPermissions/src/top/nololiyt/worldpermissions/entities/DotDividedStringBuilder.kt
@@ -2,28 +2,28 @@ package top.nololiyt.worldpermissions.entities
 
 class DotDividedStringBuilder
 {
-    private var stringBuilder: StringBuilder? = null
+    private val stringBuilder: StringBuilder;
     
     override fun toString(): String
     {
-        return stringBuilder!!.toString()
+        return stringBuilder.toString();
     }
     
     fun append(name: String): DotDividedStringBuilder
     {
-        this.stringBuilder!!.append('.').append(name)
-        return this
+        this.stringBuilder.append('.').append(name);
+        return this;
     }
     
     constructor(root: String)
     {
-        this.stringBuilder = StringBuilder()
-        this.stringBuilder!!.append(root)
+        this.stringBuilder = StringBuilder();
+        this.stringBuilder.append(root);
     }
     
     constructor(builder: DotDividedStringBuilder)
     {
-        this.stringBuilder = StringBuilder()
-        this.stringBuilder!!.append(builder.stringBuilder!!.toString())
+        this.stringBuilder = StringBuilder();
+        this.stringBuilder.append(builder.stringBuilder.toString());
     }
 }
--- NWorldPermissions/src/top/nololiyt/worldpermissions/entities/StringPair.kt
+++ NWorldPermissions/src/top/nololiyt/worldpermissions/entities/StringPair.kt
@@ -4,35 +4,34 @@ class StringPair private constructor(val key: String, val value: String)
 {
     companion object
     {
-        
         fun senderName(value: String): StringPair
         {
-            return StringPair("{senderName}", value)
+            return StringPair("{senderName}", value);
         }
         
         fun playerName(value: String): StringPair
         {
-            return StringPair("{playerName}", value)
+            return StringPair("{playerName}", value);
         }
         
         fun worldName(value: String): StringPair
         {
-            return StringPair("{worldName}", value)
+            return StringPair("{worldName}", value);
         }
         
         fun teleportedCount(value: String): StringPair
         {
-            return StringPair("{teleportedCount}", value)
+            return StringPair("{teleportedCount}", value);
         }
         
         fun unteleportedCount(value: String): StringPair
         {
-            return StringPair("{unteleportedCount}", value)
+            return StringPair("{unteleportedCount}", value);
         }
         
         fun markName(value: String): StringPair
         {
-            return StringPair("{markName}", value)
+            return StringPair("{markName}", value);
         }
     }
 }
