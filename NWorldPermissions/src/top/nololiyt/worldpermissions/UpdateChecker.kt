6eb2b3d8c2a160c5d6fbc67495076699bebc476d
yueyinqiu <lujiaqi2003@126.com>
2020-06-24 20:06:02 +0800

    优化UpdateChecker

--- NWorldPermissions/src/top/nololiyt/worldpermissions/UpdateChecker.kt
+++ NWorldPermissions/src/top/nololiyt/worldpermissions/UpdateChecker.kt
@@ -1,21 +1,13 @@
 package top.nololiyt.worldpermissions
 
-import jdk.internal.util.xml.impl.ReaderUTF8
 import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
-import org.json.JSONArray
-import org.json.JSONException
 import org.json.JSONObject
 
-import java.io.IOException
-import java.io.InputStream
-import java.io.Reader
 import java.net.URL
-import java.util.Scanner
 
 class UpdateChecker(private val plugin: Plugin)
 {
-    
     fun checkAndLog()
     {
         Bukkit.getScheduler().runTaskAsynchronously(this.plugin, Runnable {
@@ -31,7 +23,7 @@ class UpdateChecker(private val plugin: Plugin)
                         jsonObject.getJSONArray("assets").getJSONObject(0).getString("browser_download_url");
                     
                     plugin.logger.warning(
-                        "A new version: '" + tagName + "' is available. " + "It can be downloaded at '" + browserDownloadUrl + "'.");
+                        "A new version: '$tagName' is available. It can be downloaded at '$browserDownloadUrl'.");
                 }
                 
             }
