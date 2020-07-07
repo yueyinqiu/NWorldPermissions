06f367ba1ac01f6979b894d9eaa97e21890d12ca
yueyinqiu <lujiaqi2003@126.com>
2020-07-07 17:01:29 +0800

    优化 marks remove

--- NWorldPermissions/src/top/nololiyt/worldpermissions/commands/marks/RemoveExecutor.java
+++ NWorldPermissions/src/top/nololiyt/worldpermissions/commands/marks/RemoveExecutor.java
@@ -4,6 +4,7 @@ import org.bukkit.command.CommandSender;
 import top.nololiyt.worldpermissions.configurationmanagers.MarksManager;
 import top.nololiyt.worldpermissions.RootPlugin;
 import top.nololiyt.worldpermissions.entitiesandtools.DotDividedStringBuilder;
+import top.nololiyt.worldpermissions.entitiesandtools.MessagesSender;
 import top.nololiyt.worldpermissions.entitiesandtools.StringPair;
 import top.nololiyt.worldpermissions.commands.Executor;
 
@@ -32,35 +33,30 @@ public class RemoveExecutor extends Executor
     {
         if (args.length - 1 != layer)
             return false;
-        
-        StringPair[] cPairs = new StringPair[]{
+    
+        MessagesSender messagesSender = new MessagesSender(rootPlugin.getMessagesManager(),
+                commandSender,new StringPair[]{
                 StringPair.markName(args[layer]),
                 StringPair.senderName(commandSender.getName())
-        };
+        });
+        
         try
         {
             MarksManager marksManager = rootPlugin.getMarksManager();
-            
             if(marksManager.getMark(args[layer]) == null)
             {
-                rootPlugin.getMessagesManager().sendMessage(
-                        commandSender, messageKey.append("no-such-mark"), cPairs);
+                messagesSender.send(messageKey.append("no-such-mark"));
                 return true;
             }
-    
             marksManager.setMark(args[layer], null);
+            messagesSender.send(messageKey.append("completed"));
+            return true;
         }
         catch(IOException ex)
         {
             ex.printStackTrace();
-    
-            rootPlugin.getMessagesManager().sendMessage(
-                    commandSender, messageKey.append("failed"), cPairs);
+            messagesSender.send(messageKey.append("failed"));
             return true;
         }
-    
-        rootPlugin.getMessagesManager().sendMessage(
-                commandSender, messageKey.append("completed"), cPairs);
-        return true;
     }
 }
