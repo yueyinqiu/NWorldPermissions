2f534d9699a2f175b066b73477e646ca81f1d407
yueyinqiu <lujiaqi2003@126.com>
2020-07-07 17:46:52 +0800

    优化Online

--- NWorldPermissions/src/top/nololiyt/worldpermissions/commands/tp/OnlineExecutor.java
+++ NWorldPermissions/src/top/nololiyt/worldpermissions/commands/tp/OnlineExecutor.java
@@ -7,7 +7,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import top.nololiyt.worldpermissions.RootPlugin;
 import top.nololiyt.worldpermissions.commands.Executor;
+import top.nololiyt.worldpermissions.configurationmanagers.MessagesManager;
 import top.nololiyt.worldpermissions.entitiesandtools.DotDividedStringBuilder;
+import top.nololiyt.worldpermissions.entitiesandtools.MessagesSender;
 import top.nololiyt.worldpermissions.entitiesandtools.StringPair;
 
 import java.util.List;
@@ -33,118 +35,142 @@ public class OnlineExecutor extends Executor
                           DotDividedStringBuilder messageKey, CommandSender commandSender,
                           String[] args)
     {
-        int times;
-        String sTimes;
-        if (args.length - 1 != layer + 1)
-        {
-            if (args.length - 1 != layer + 2)
-            {
-                return false;
-            }
-            sTimes = args[layer + 2];
-            try
-            {
-                times = Integer.parseInt(sTimes);
-            }
-            catch (NumberFormatException ex)
-            {
-                return false;
-            }
-        }
-        else
-        {
-            times = 1;
-            sTimes = "1";
-        }
+        Times times = getTimes(args, layer);
+        if (times == null)
+            return false;
     
         String worldName = args[layer];
         String markName = args[layer + 1];
-        
+    
         String senderName = commandSender.getName();
-        
-        StringPair[] basePairs = new StringPair[]{
+    
+        MessagesManager messagesManager = rootPlugin.getMessagesManager();
+        MessagesSender messagesSender = new MessagesSender(
+                messagesManager, commandSender, new StringPair[]{
                 StringPair.markName(markName),
                 StringPair.worldName(worldName),
                 StringPair.senderName(senderName),
-                StringPair.teleportationTimes(sTimes)
-        };
+                StringPair.teleportationTimes(times.stringValue)
+        });
     
         World world = Bukkit.getWorld(args[layer]);
         if (world == null)
         {
-            rootPlugin.getMessagesManager().sendMessage(
-                    commandSender, messageKey.append("no-such-world"), basePairs);
+            messagesSender.send("no-such-world");
             return true;
         }
     
         Location location = rootPlugin.getMarksManager().getMark(markName);
         if (location == null)
         {
-            rootPlugin.getMessagesManager().sendMessage(
-                    commandSender, messageKey.append("no-such-mark"), basePairs);
+            messagesSender.send("no-such-mark");
             return true;
         }
-        
+    
         List<Player> players = world.getPlayers();
     
-        StringPair[] playersPairs = new StringPair[]{
-                null,
-                StringPair.markName(markName),
-                StringPair.worldName(worldName),
-                StringPair.senderName(senderName),
-                StringPair.teleportationTimes(sTimes)
-        };
-    
-        StringPair[] playersFailPairs = new StringPair[]{
+        messagesSender.setArgs(new StringPair[]{
                 null, null,
                 StringPair.markName(markName),
                 StringPair.worldName(worldName),
                 StringPair.senderName(senderName),
-                StringPair.teleportationTimes(sTimes)
-        };
-        
-        int sCount = 0, fCount = 0;
-        DotDividedStringBuilder cKey = new DotDividedStringBuilder(messageKey);
+                StringPair.teleportationTimes(times.stringValue)
+        });
+    
+        DotDividedStringBuilder messageKeyCopy = new DotDividedStringBuilder(messageKey);
         messageKey.append("failed-to-teleport-someone");
+    
+        TeleportCount count =
+                teleport(players, location, times.intValue, messageKey, messagesSender,
+                        new MessagesSender(messagesManager, null, new StringPair[]{
+                                null,
+                                StringPair.markName(markName),
+                                StringPair.worldName(worldName),
+                                StringPair.senderName(senderName),
+                                StringPair.teleportationTimes(times.stringValue)
+                        }));
+    
+        messagesSender.setArgs(new StringPair[]{
+                StringPair.teleportedCount(String.valueOf(count.success)),
+                StringPair.unteleportedCount(String.valueOf(count.failure)),
+                StringPair.markName(markName),
+                StringPair.worldName(worldName),
+                StringPair.senderName(senderName),
+                StringPair.teleportationTimes(times.stringValue)
+        });
+        messagesSender.send(messageKeyCopy.append("completed"));
+        return true;
+    }
+    
+    class Times
+    {
+        int intValue;
+        String stringValue;
+    }
+    
+    private Times getTimes(String[] args, int layer)
+    {
+        Times times = new Times();
+        
+        if (args.length - 1 != layer + 1)
+        {
+            if (args.length - 1 != layer + 2)
+                return null;
+            
+            times.stringValue = args[layer + 2];
+            try
+            {
+                times.intValue = Integer.parseInt(times.stringValue);
+            }
+            catch (NumberFormatException ex)
+            {
+                return null;
+            }
+        }
+        else
+        {
+            times.intValue = 1;
+            times.stringValue = "1";
+        }
+        return times;
+    }
+    
+    class TeleportCount
+    {
+        int success;
+        int failure;
+    }
+    
+    private TeleportCount teleport(List<Player> players, Location mark,
+                                   int times, DotDividedStringBuilder failMessageKey,
+                                   MessagesSender toSender, MessagesSender toPlayers)
+    {
+        TeleportCount count = new TeleportCount();
+        count.success = 0;
+        count.failure = 0;
+        DotDividedStringBuilder teleportMessage = new DotDividedStringBuilder(
+                "messages.to-players.when-teleported-by-tp-online.before-teleport");
         for (Player player : players)
         {
-            playersFailPairs[0] = playersPairs[0]
+            toPlayers.getArgs()[0] = toSender.getArgs()[0]
                     = StringPair.playerName(player.getDisplayName());
-    
-    
-            rootPlugin.getMessagesManager().sendMessage(
-                    player, new DotDividedStringBuilder(
-                            "messages.to-players.when-teleported-by-tp-online.before-teleport"), playersPairs
-            );
+            toPlayers.setTarget(player);
+            toPlayers.send(teleportMessage);
     
             int realTimes = 0;
             for (int i = 0; i < times; i++)
             {
-                realTimes += player.teleport(location) ? 1 : 0;
+                realTimes += player.teleport(mark) ? 1 : 0;
             }
-    
             if (realTimes < times)
             {
-                fCount++;
-                playersFailPairs[1] = StringPair.teleportedTimes(String.valueOf(realTimes));
-                rootPlugin.getMessagesManager().sendMessage(commandSender, messageKey, playersFailPairs);
+                count.failure++;
+                toSender.getArgs()[1] = StringPair.teleportedTimes(String.valueOf(realTimes));
+                toSender.send(failMessageKey);
+                continue;
             }
-            else
-            {
-                sCount++;
-            }
+            count.success++;
         }
-    
-        StringPair[] cPairs = new StringPair[]{
-                StringPair.teleportedCount(String.valueOf(sCount)),
-                StringPair.unteleportedCount(String.valueOf(fCount)),
-                StringPair.markName(markName),
-                StringPair.worldName(worldName),
-                StringPair.senderName(senderName),
-                StringPair.teleportationTimes(sTimes)
-        };
-        rootPlugin.getMessagesManager().sendMessage(
-                commandSender, cKey.append("completed"), cPairs);
-        return true;
+        return count;
     }
 }
