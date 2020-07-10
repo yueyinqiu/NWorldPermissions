a5165f235ce3eca39a453e179306ad5c103b9ac5
yueyinqiu <lujiaqi2003@126.com>
2020-07-10 16:47:39 +0800

    添加placeholder支持

--- NWorldPermissions/src/top/nololiyt/worldpermissions/configurationmanagers/MessagesManager.java
+++ NWorldPermissions/src/top/nololiyt/worldpermissions/configurationmanagers/MessagesManager.java
@@ -1,8 +1,13 @@
 package top.nololiyt.worldpermissions.configurationmanagers;
 
 import com.sun.istack.internal.NotNull;
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import top.nololiyt.worldpermissions.RootPlugin;
 import top.nololiyt.worldpermissions.entitiesandtools.DotDividedStringBuilder;
 import top.nololiyt.worldpermissions.entitiesandtools.StringPair;
@@ -21,7 +26,8 @@ public class MessagesManager extends ConfigurationManager
         return "messages.yml";
     }
     
-    public @NotNull String getItemAndShowError(DotDividedStringBuilder node)
+    public @NotNull
+    String getItemAndShowError(DotDividedStringBuilder node)
     {
         String key = node.toString();
         String result = getConfiguration().getString(key);
@@ -47,11 +53,21 @@ public class MessagesManager extends ConfigurationManager
         String result = message.trim();
         if (result.isEmpty())
             return;
+        
         result = ChatColor.translateAlternateColorCodes('&', result);
         for (StringPair pair : args)
         {
             result = result.replace(pair.getKey(), pair.getValue());
         }
+        
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null)
+        {
+            if (target instanceof Player)
+                result = PlaceholderAPI.setPlaceholders((Player) target, result);
+            else if (target instanceof OfflinePlayer)
+                result = PlaceholderAPI.setPlaceholders((OfflinePlayer) target, result);
+        }
+        
         target.sendMessage(result);
     }
 }
